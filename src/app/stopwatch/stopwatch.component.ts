638368a9ea427892fd6f9591c6030346c3d028fb
yurii19 <yurikolechkin@gmail.com>
2023-06-09 21:42:35 +0300

    chore: added stopwatch component

---
+++ src/app/stopwatch/stopwatch.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-stopwatch',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './stopwatch.component.html',
+  styleUrls: ['./stopwatch.component.css']
+})
+export class StopwatchComponent {
+
+}
