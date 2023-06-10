d781652dedef9274fd387bdfe2f73dc16151baa1
yurii19 <yurikolechkin@gmail.com>
2023-06-10 08:57:00 +0300

    chore: controls added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import {StopwatchComponent} from './stopwatch/stopwatch.component'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   standalone: true,
+  imports:[StopwatchComponent]
 })
 export class AppComponent {
   title = 'iteam-stopwatch';
